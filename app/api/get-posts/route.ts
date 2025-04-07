c6d8a29ae6bc104b6dc4a70d24d5871bb22ffe7e
Saddam Ahmed <saddam.groot@gmail.com>
2025-04-07 12:24:47 +0530

    Update route.ts
    reset back to original code

--- app/api/get-posts/route.ts
+++ app/api/get-posts/route.ts
@@ -1,37 +1,8 @@
-import {NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import {posts} from '../data/posts';
 
-// interface Post {
-//     id: number;
-//     title: string;
-//     content: string;
-//     createdAt: string;
-// }
-
-// const posts:Post[] = [
-//     {
-//         id:1,
-//         title: "First Post",
-//         content:"The is the First Post created for sample data",
-//         createdAt: new Date().toISOString(),
-//     },
-// ];
 
 
-export async function GET(request:Request) {
-  
-  const { searchParams } = new URL(request.url);
-  const tag = searchParams.get('tag') || '';
-
-  if(tag){
-    const newPosts = posts.map((post) => post.tag===tag)
-    return NextResponse.json(newPosts);
-  }
-  else{
-    return NextResponse.json(posts);
-  }
+export async function GET() {
+  return NextResponse.json(posts);
 }
-
-// export async function POST(){
-  
-// }
