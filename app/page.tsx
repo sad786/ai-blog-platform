d8ba31d999e96fe28cdac7d578c4c9a8ab040ab3
Saddam Ahmed <saddam.groot@gmail.com>
2025-04-07 12:23:37 +0530

    Update page.tsx
    reset to original code

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ interface Post {
   title: string;
   content: string;
   createdAt: string;
-  tag:string;
 }
 
 export default function Home() {
@@ -24,14 +23,6 @@ export default function Home() {
     fetchPosts();
 //    setLoading(false);
   },[]);
-
-
-  const handleTab = async (tag:string) => {
-    //const id = await fetch("api/get-posts/");
-    const res = await fetch(`api/get-posts?tag=${tag}`);
-    const posts = await res.json();
-    setPosts(posts);
-  }
 
   const fetchPosts = async () => {
     const response = await axios.get<Post[]>('/api/get-posts');
@@ -51,41 +42,6 @@ export default function Home() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-8">AI Blogging Platform</h1>
-      <button
-            type="button"
-            onClick={handleTab}
-            style={{
-              padding: "10px 20px",
-              backgroundColor: loading ? "#ccc" : "#007bff",
-              color: "#fff",
-              border: "none",
-              borderRadius: "4px",
-              cursor: loading ? "not-allowed" : "pointer",
-              marginTop:"10px",
-              opacity: loading ? 0.6 : 1
-            }}
-            className="w-64 py-3 px-6 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all disabled:bg-gray-400"
-          >
-        Nature
-        </button>
-        <button
-            type="button"
-            onClick={handleTab}
-            style={{
-              padding: "10px 20px",
-              backgroundColor: loading ? "#ccc" : "#007bff",
-              color: "#fff",
-              border: "none",
-              borderRadius: "4px",
-              cursor: loading ? "not-allowed" : "pointer",
-              marginTop:"10px",
-              opacity: loading ? 0.6 : 1
-            }}
-            className="w-64 py-3 px-6 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all disabled:bg-gray-400"
-          >
-
-          AI</button>
-
       <SearchBar setSearchTerm={setSearchTerm} />
 
 
